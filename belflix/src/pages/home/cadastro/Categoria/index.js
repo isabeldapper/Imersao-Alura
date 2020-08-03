e040ebf9483c88abccddf69c4c596c0ec40fff17
Isabel <icristinadapper@gmail.com>
2020-08-03 14:28:12 -0300

    aula 03

--- belflix/src/pages/home/cadastro/Categoria/index.js
+++ belflix/src/pages/home/cadastro/Categoria/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../../../components/PageDefault/index';
-import FormField from '../../../../components/FormField/index';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
   const valoresIniciais = {
